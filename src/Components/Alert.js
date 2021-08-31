3cb966b943867b6e9a392a7b04ccca7e661f91f6
inamfalahuddin <inamfalahuddin06@gmail.com>
2021-08-31 21:13:24 +0700

    meperbaiki kalimat typo

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -3,7 +3,7 @@ import React from "react";
 function Alert({ nama }) {
   return (
     <div className="card-alert fadeIn ">
-      <h4>Hello, {nama}</h4>
+      <h4>Halo, {nama}</h4>
       <h4>
         Data Anda Berhasil Kami Terima. Terimakasih Atas Partisipasinya Dalam
         PKKMB FTI 2021
