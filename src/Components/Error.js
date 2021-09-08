e33b8b5d044c2462d7a746eb645b2a1573c1dff2
inamfalahuddin <inamfalahuddin06@gmail.com>
2021-09-08 15:05:27 +0700

    menbambah beberapa indent pada page Error.js

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Error(error) {
   return (
-    <div className="card-alert fadeIn">
+    <div className="card-alert fadeIn  my-5 py-5 ">
       <h4>Terjadi Keslahan !</h4>
       <p>Silahkan Periksa Jaringan Anda.</p>
     </div>
